fd4b2ca82ab172d5b621f89ac969e61da4fbf500
adcadams <andrewdcadams@gmail.com>
2021-04-25 12:46:10 -0400

    sets up cors. adds passing componentdidmount test

--- escrow-frontend/src/App.js
+++ escrow-frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount(){
+    fetch('http://localhost:3000/contracts')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
+
   render(){
   return (
     <div className="App">
-      
+      app
     </div>
   );
 }
