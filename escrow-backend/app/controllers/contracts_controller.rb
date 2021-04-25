34985f4d36b9ec7612022686f850ca53f4b4f7a2
adcadams <andrewdcadams@gmail.com>
2021-04-25 11:33:57 -0400

    adds methods to contracts controller

--- escrow-backend/app/controllers/contracts_controller.rb
+++ escrow-backend/app/controllers/contracts_controller.rb
@@ -1,2 +1,41 @@
 class ContractsController < ApplicationController
+
+    def index
+        @contracts = Contract.all
+        render json: @contracts
+      end
+  
+      def create
+        @contract = Contract.new(contract_params)
+        if @contract.save
+          render json: @contract
+        else
+          render json: {error: 'Error creating contract'}
+        end
+      end
+  
+      def show
+        @contract = Contract.find(params[:id])
+        render json: @contract
+      end
+  
+      def destroy
+        @contract = Contract.find(params[:id])
+        @contract.destroy
+      end
+  
+    #   def update
+    #     @contract = Contract.find(params[:id])
+    #     @contract.update(name: params["contract"]["name"])
+    #     @contract.save
+    #     render json: @contract
+    #   end
+  
+      private
+  
+      def contract_params
+        params.require(:contract).permit(:buyer_address, :seller_address, :amount)
+      end
+
+
 end
