fa641031ea9a8ab82bbd155ccd48114b8b00a33c
adcadams <andrewdcadams@gmail.com>
2021-04-29 10:58:41 -0400

    adds link to contracts/id in contracts

--- escrow-frontend/src/components/Contracts.js
+++ escrow-frontend/src/components/Contracts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import '../CSS/contracts.css'
 // import Contract from 'Contract'
 
@@ -12,8 +13,9 @@ const Contracts = (props) => {
             {props.contracts.map(contract => 
             
             <span key={contract.id} className="one-contract-index-span">
+                <Link to={`/contracts/${contract.id}`}>Contract ID: {contract.id}</Link>
                 <ul key={contract.id}>
-            
+                
                 <li key={contract.buyer_address}>Buyer - {contract.buyer} - {contract.buyer_address} </li>
                 <li key={contract.seller_address}>Seller - {contract.seller} - {contract.seller_address}</li>
                 <li key={contract.amount}>Amount - {contract.amount}</li>
